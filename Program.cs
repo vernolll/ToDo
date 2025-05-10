9ff60478f733d798f7162ae4a8c320ec351ec25b
Марина Карась <marina2006.karas@yandex.ru>
2025-05-10 14:00:11 +0300

    no advertisement

--- Program.cs
+++ Program.cs
@@ -4,26 +4,22 @@ using TodoApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Настройка контекста базы данных
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDbContext<TodoContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Настройка Identity с UI
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
 })
 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-// Подключение Razor Pages
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Конфигурирование конвейера HTTP запросов
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -35,9 +31,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication(); // Аутентификация
-app.UseAuthorization();  // Авторизация
+app.UseAuthentication(); 
+app.UseAuthorization(); 
 
-app.MapRazorPages();  // Маппинг Razor Pages
+app.MapRazorPages();  
 
 app.Run();
